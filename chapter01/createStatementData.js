e2c8b67c028667643104b17583dfed5eb2de095b
Msksgm <msksgm3124@gmail.com>
2022-06-25 07:25:14 +0900

    fix: ボリューム特典ポイントの計算を条件記述を置き換え

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -48,11 +48,7 @@ class PerformanceCalculator {
     throw new Error("サブクラスの責務");
   }
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -85,6 +81,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
