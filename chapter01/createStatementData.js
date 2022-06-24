f9a6a3c93caea637fbe52eadbdc8ab88f5bc0182
Msksgm <msksgm3124@gmail.com>
2022-06-25 07:03:54 +0900

    fix: p.38

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -31,25 +31,8 @@ function createStatementData(invoice, plays) {
     return result;
   }
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
@@ -58,7 +41,7 @@ function createStatementData(invoice, plays) {
 
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
-    this.perfomance = aPerformance;
+    this.performance = aPerformance;
     this.play = aPlay;
   }
   get amount() {
