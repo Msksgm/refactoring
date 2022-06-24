d7218173043aeac71396536589849ad4d3eed19d
Msksgm <msksgm3124@gmail.com>
2022-06-25 06:36:07 +0900

    fix: p.31

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -5,24 +5,27 @@ function statement(invoice, plays) {
 }
 
 function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`;
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
   for (let perf of data.perfomances) {
     // 注文の内訳を出力
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
-      perf.audience
-    } seats)\n`;
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td><tr>\n`;
   }
-  result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  console.log(result);
   return result;
+}
 
-  function usd(aNumber) {
-    return Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
+function usd(aNumber) {
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 module.exports = statement;
--- chapter01/test/statement.test.js
+++ chapter01/test/statement.test.js
@@ -6,12 +6,14 @@ const statement = require("../statement");
 describe("statement test", () => {
   it("compare statement", () => {
     const actual = statement(invoices, plays);
-    const mock = `Statement for BigCo
- Hamlet: $650.00 (55 seats)
- As You Like it: $580.00 (35 seats)
- Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
+    const mock = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td><tr>
+ <tr><td>As You Like it</td><td>35</td><td>$580.00</td><tr>
+ <tr><td>Othello</td><td>40</td><td>$500.00</td><tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
 `;
     expect(actual).toBe(mock);
   });
