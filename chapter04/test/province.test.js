fd67e46bf5b35059615c7ca04a3ec0dfb438644d
Msksgm <msksgm3124@gmail.com>
2022-06-29 07:16:48 +0900

    add: p.101 境界値の検査

--- chapter04/test/province.test.js
+++ chapter04/test/province.test.js
@@ -1,19 +1,55 @@
 const Province = require("../province");
 
 describe("province", () => {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
+
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
+
   it("change production", function () {
-    const asia = new Province(sampleProvinceData());
     asia.producers[0].production = 20;
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
+  });
+
+  it("zero demand", function () {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+
+  it("negative demand", function () {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+});
+
+describe("no producers", function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", function () {
+    expect(noProducers.shortfall).toBe(30);
+  });
+
+  it("profit", function () {
+    expect(noProducers.profit).toBe(0);
   });
 });
 
